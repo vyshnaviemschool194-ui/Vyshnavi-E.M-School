f8281e2b6f1361e0aab55bee3e798608b502da23
vyshnaviemschool194-ui <vyshnaviemschool194@gmail.com>
2026-02-05 20:53:30 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -580,17 +580,3 @@ window.onload = function () {
 function closePopup() {
   document.getElementById("admissionPopup").style.display = "none";
 }
-
-
-<script>
-document.addEventListener("DOMContentLoaded", function () {
-    const hamburger = document.querySelector(".hamburger");
-    const navMenu = document.querySelector(".nav-menu");
-
-    if (hamburger && navMenu) {
-        hamburger.addEventListener("click", () => {
-            navMenu.classList.toggle("active");
-        });
-    }
-});
-</script>
